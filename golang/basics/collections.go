0520c03b63e99983aba62a6df519e6ec4e9af9b6
bhavik <bhavik@work>
2023-05-08 20:11:55 -0500

    wip

--- golang/basics/collections.go
+++ golang/basics/collections.go
@@ -2,15 +2,20 @@ package basics
 
 import (
 	"fmt"
+
+	"golang.org/x/exp/slices"
 )
 
 func DemosForCollectionsInGoLang() {
 	fmt.Print("\033[H\033[2J")
-	PrintHeader("GO CLI APP: Collections")
+	PrintHeader("Collections")
 	demoArray()
+	demoSlices()
+	demoMaps()
 }
 
 func demoArray() {
+	PrintTitle("ARRAYS")
 	var array [3]int
 	fmt.Println(array)
 	array = [3]int{1, 2, 3}
@@ -29,4 +34,73 @@ func demoArray() {
 	fmt.Println(copiedArray)
 
 	fmt.Println(originalArray == copiedArray)
+}
+
+func demoSlices() {
+	PrintTitle("SLICES")
+	var orgSlice []string //slices don't have set size
+	fmt.Println(orgSlice) //default value is empty string, or 0
+
+	orgSlice = []string{"Coffee", "Espresso", "Cappuccino"}
+	fmt.Print("Org Slice: ")
+	fmt.Println(orgSlice)
+
+	fmt.Print("Org Slice at 1: ")
+	fmt.Println(orgSlice[1])
+	orgSlice[1] = "ChaiTea"
+	fmt.Print("Org Slice: ")
+	fmt.Println(orgSlice)
+	cpSlice := orgSlice
+	fmt.Print("CP Slice: ")
+	fmt.Println(cpSlice)
+	cpSlice[1] = "ChaiLatte"
+	fmt.Println("Updated CP Slice at 1: ChaiLatte")
+	fmt.Print("Org Slice: ")
+	fmt.Println(orgSlice)
+	fmt.Print("CP Slice: ")
+	fmt.Println(cpSlice)
+
+	AddSeparator()
+
+	fmt.Printf("Org Slice Ref: %p", &orgSlice)
+	AddSeparator()
+	fmt.Printf("CP Slice Ref: %p", &cpSlice)
+	AddSeparator()
+	AddSeparator()
+	for i := 0; i < len(orgSlice); i++ {
+		fmt.Printf("[%s]: %p", orgSlice[i], &orgSlice[i])
+		AddSeparator()
+	}
+	AddSeparator()
+	for i := 0; i < len(cpSlice); i++ {
+		fmt.Printf("[%s]: %p", cpSlice[i], &cpSlice[i])
+		AddSeparator()
+	}
+	AddSeparator()
+	orgSlice = append(orgSlice, "HotChocolate", "HotTea")
+	fmt.Print("Org Slice: ")
+	fmt.Println(orgSlice)
+	fmt.Print("CP Slice:")
+	fmt.Println(cpSlice)
+	PrintNotes(`
+		When orgSlice was mutated, cpSlice started a new life and decoupled itself when append(...) was used 
+	`)
+
+	for i := 0; i < len(orgSlice); i++ {
+		fmt.Printf("[%s]: %p", orgSlice[i], &orgSlice[i])
+		AddSeparator()
+	}
+	AddSeparator()
+	for i := 0; i < len(cpSlice); i++ {
+		fmt.Printf("[%s]: %p", cpSlice[i], &cpSlice[i])
+		AddSeparator()
+	}
+	AddSeparator()
+
+	slices.Delete(orgSlice, 1, 3)
+	fmt.Println(orgSlice)
+}
+
+func demoMaps() {
+	PrintTitle("MAPS")
 }
--- golang/go.mod
+++ golang/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 )
