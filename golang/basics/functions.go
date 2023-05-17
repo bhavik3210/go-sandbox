91e9eae3beb0c39ea37f603e99709f52d6ecd650
bhavik <bhavik@work>
2023-05-17 08:15:40 -0500

    wip

--- golang/basics/functions.go
+++ golang/basics/functions.go
@@ -8,6 +8,7 @@ func functionName(incoming values)(outgoing values if any){}
 */
 func DemoFunctions() {
 	demoParametersAndArguments()
+	demoFucntionWithMultipleReturnTypes()
 }
 
 func greet(name1 string, name2 string) {
@@ -37,4 +38,19 @@ func demoParametersAndArguments() {
 	conciesParamas("name1", "name2")
 	variadicParamenters()
 	variadicParamenters("name1", "name2", "name3", "name4", "nameN")
+}
+
+func divide(l, r int) (int, bool) {
+	if r == 0 {
+		return 0, false
+	}
+	return l / r, true
+}
+
+func demoFucntionWithMultipleReturnTypes() {
+	PrintTitle("Multiple Return Types/Values")
+	result, ok := divide(1, 2)
+	if ok {
+		fmt.Println(result)
+	}
 }
