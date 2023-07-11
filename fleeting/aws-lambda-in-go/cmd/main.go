bb7f4f193f9d8538fe2b628f3fc1098f37452085
bhavik <bhavik@work>
2023-07-10 19:09:44 -0500

    wip

--- fleeting/aws-lambda-in-go/cmd/main.go
+++ fleeting/aws-lambda-in-go/cmd/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	dynaClient dynamodbiface.DynamoDBAPI
 )
 
 func main() {
 	region := "us-east-2"
-	session.NewSession(&aws.Config{
+	awsSession, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
 	})
 
+	if err != nil {
+		return
+	}
+	dynaClient = dynamodb.New(awsSession)
+	lambda.Start(handler)
 }
